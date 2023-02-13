5b86e28eb7500a3875c9cbb660b577330483e9dd
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-02-13 09:59:23 -0500

    default theme to white

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
 					--font-sans: ${fontSans.style.fontFamily};
 				}
 			}`}</style>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
