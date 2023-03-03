58a05dcc756ed0fd8e78df18cf4d637a9d62e639
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-03-03 13:46:52 -0500

    better query handling

--- components/search/search-index-button.tsx
+++ components/search/search-index-button.tsx
@@ -1,24 +1,33 @@
-"use client"
+'use client';
+
 import { useRouter } from 'next/navigation';
-import { Button } from "@/components/ui/button";
+
+import { Button } from '@/components/ui/button';
 
 interface SearchIndexButtonProps {
-  index: string,
-  params?: any,
-  name: string,
-  label: string
+  index: string;
+  params?: any;
+  name: string;
+  label: string;
 }
 
-export function SearchIndexButton({ index, params, name, label }: SearchIndexButtonProps) {
+export function SearchIndexButton({
+  index,
+  params,
+  name,
+  label,
+}: SearchIndexButtonProps) {
   const router = useRouter();
 
   function buttonClick() {
     if (index !== name) {
-      console.log('go to index: ' + name)
-      let qs = name === 'collections' ? `hasPhoto=true` : '';
-      qs += qs && params?.q ? '&' : ''
-      qs += params?.q ? `q=${params?.q}` : '';
-      router.push(`/search/${name}?${qs}`)
+      console.log('go to index: ' + name);
+      const newParams = new URLSearchParams();
+      if (name === 'collections') newParams.set('hasPhoto', 'true');
+      if (params?.q) newParams.set('q', params.q);
+      if (params?.layout) newParams.set('layout', params.layout);
+      if (params?.f) newParams.set('f', params.f);
+      router.push(`/search/${name}?${newParams}`);
     }
   }
 
@@ -33,5 +42,5 @@ export function SearchIndexButton({ index, params, name, label }: SearchIndexBut
         {label}
       </Button>
     </>
-  )
+  );
 }
