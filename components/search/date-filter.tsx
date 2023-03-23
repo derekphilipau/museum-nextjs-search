78d32df24dc3fc721463df8f48f60108fe6f714e
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-03-23 15:17:14 -0400

    refactor dates debounce

--- components/search/date-filter.tsx
+++ components/search/date-filter.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import { getDictionary } from '@/dictionaries/dictionaries';
-import { getBooleanValue } from '@/util/various';
-import { ChevronsUpDown, Plus, X } from 'lucide-react';
+import { ChevronsUpDown } from 'lucide-react';
 
-import type { AggOption } from '@/types/aggOption';
+import { useDebounce } from '@/components/forms/debounce';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import {
   Collapsible,
   CollapsibleContent,
@@ -26,33 +24,34 @@ export function DateFilter({ params }: DateFilterProps) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const [fromDate, setFromDate] = useState('');
-  const [toDate, setToDate] = useState('');
-  const [realFromDate, setRealFromDate] = useState('');
-  const [realToDate, setRealToDate] = useState('');
+  const [fromDate, setFromDate] = useState(params?.startDate || '');
+  const [toDate, setToDate] = useState(params?.endDate || '');
   const [isOpen, setIsOpen] = useState(false);
 
   const displayName = dict['index.collections.date'];
   const fromDateName = dict['index.collections.date.from'];
   const toDateName = dict['index.collections.date.to'];
 
-  useEffect(() => {
-    const debounceQuery = setTimeout(() => {
-      setRealFromDate(fromDate);
-      setRealToDate(toDate);
-    }, 800);
-    return () => clearTimeout(debounceQuery);
-  }, [fromDate, toDate]);
-
-  useEffect(() => {
-    if (!realFromDate || !realToDate) return;
+  const debouncedRequest = useDebounce(() => {
+    if (!fromDate && !toDate) return;
     const updatedParams = new URLSearchParams(params);
-    if (realFromDate) updatedParams.set('startDate', realFromDate);
-    if (realToDate) updatedParams.set('endDate', realToDate);
+    if (fromDate) updatedParams.set('startDate', fromDate);
+    else updatedParams.delete('startDate');
+    if (toDate) updatedParams.set('endDate', toDate);
+    else updatedParams.delete('endDate');
     updatedParams.delete('p');
-    console.log('date go: ' + realFromDate + ' ' + realToDate);
     router.push(`${pathname}?${updatedParams}`);
-  }, [realFromDate, realToDate, params, pathname, router]);
+  });
+
+  const onFromDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFromDate(e.target.value);
+    debouncedRequest();
+  };
+
+  const onToDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setToDate(e.target.value);
+    debouncedRequest();
+  };
 
   return (
     <Collapsible
@@ -81,10 +80,11 @@ export function DateFilter({ params }: DateFilterProps) {
               {fromDateName}
             </Label>
             <Input
+              className="w-20"
               name="fromDate"
               placeholder={'YYYY'}
-              onChange={(e) => setFromDate(e.target.value)}
-              className="w-24"
+              onChange={onFromDateChange}
+              value={fromDate}
             />
           </div>
           <div>
@@ -92,10 +92,11 @@ export function DateFilter({ params }: DateFilterProps) {
               {toDateName}
             </Label>
             <Input
+              className="w-20"
               name="toDate"
               placeholder={'YYYY'}
-              onChange={(e) => setToDate(e.target.value)}
-              className="w-24"
+              onChange={onToDateChange}
+              value={toDate}
             />
           </div>
         </div>
