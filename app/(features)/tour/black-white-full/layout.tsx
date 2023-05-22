7c83a8918e2d077ba301f7099be976fe2af0857c
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-05-22 16:24:10 -0400

    add full-size gallery

---
+++ app/(features)/tour/black-white-full/layout.tsx
@@ -0,0 +1,46 @@
+import { Inter } from 'next/font/google';
+
+import { SiteHeader } from '@/components/layout/site-header';
+import '../../../globals.css';
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
+export const metadata = {
+  title: {
+    default: 'My Museum',
+    template: '%s | My Museum',
+  },
+  description: 'Elasticsearch + Next.js Search Prototype',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: '/favicon.ico',
+        media: '(prefers-color-scheme: dark)',
+      },
+    ],
+  },
+};
+
+export default async function FeatureLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head />
+      <body className="bg-white font-sans text-neutral-900 antialiased dark:bg-neutral-900 dark:text-neutral-50">
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
