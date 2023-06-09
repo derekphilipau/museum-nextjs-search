94a8c8d83cbacda005de517987377aeef0fa4cde
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-06-09 16:02:09 -0400

    make museum map self-contained

--- components/museum-map/museum-map-dialog.tsx
+++ components/museum-map/museum-map-dialog.tsx
@@ -14,29 +14,31 @@ import { MuseumMap } from './museum-map';
 export function MuseumMapDialog({ item }: { item: CollectionObjectDocument }) {
   const [open, setOpen] = useState(false);
 
-  if (!item?.id || !item?.museumLocation) return null;
+  if (
+    !item?.id ||
+    !item?.museumLocation ||
+    item?.museumLocation === 'This item is not on view'
+  )
+    return null;
 
   return (
     <>
-      <div
-        onClick={() => setOpen(true)}
-        className="h-80 cursor-pointer"
-      >
-        <h4 className="mb-4 text-sm font-semibold uppercase text-neutral-500 dark:text-neutral-400">Museum Map</h4>
+      <div onClick={() => setOpen(true)} className="h-80 cursor-pointer">
+        <h4 className="mb-4 text-sm font-semibold uppercase text-neutral-500 dark:text-neutral-400">
+          Museum Map
+        </h4>
         <MuseumMap item={item} />
       </div>
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className="h-full min-w-full">
           <DialogHeader>
-            <DialogTitle className="z-50">
-                {item.title}
-            </DialogTitle>
+            <DialogTitle className="z-50">{item.title}</DialogTitle>
             <div className="h-screen overflow-y-scroll">
-                <div className="w-full">
-                  <div className="flex max-w-md justify-center">
-                    <MuseumMap item={item} />
-                  </div>
+              <div className="w-full">
+                <div className="flex max-w-md justify-center">
+                  <MuseumMap item={item} />
                 </div>
+              </div>
             </div>
           </DialogHeader>
         </DialogContent>
