a57a9385d95e208d5429efcd956c11a50780b671
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-06-12 19:00:03 -0400

    begin cloud function, get data from gzipped urls

--- util/data/dataFiles.ts
+++ util/data/dataFiles.ts
@@ -1,12 +1,9 @@
-/**
- * Define locations of data files
- */
 
-export const collectionsDataFile = './data/BrooklynMuseum/collections.jsonl'; // Entire collection ~100,000 records
-export const contentDataFile = './data/BrooklynMuseum/content.jsonl';
+/*
+ * The following should be deprecated in favor of URL accessed datafiles.
+ */
 export const termsDataFile = './data/BkM/json/terms.jsonl';
 export const artistTermsDataFile = './data/BkM/json/artistTerms.jsonl';
-export const archivesDataFile = './data/BrooklynMuseum/archivesSpaceDCRecords.jsonl';
 
 export const ulanArtistsFile = './data/ULAN/json/ulanArtists.jsonl';
 export const ulanCorporateBodiesFile =
@@ -19,3 +16,17 @@ export const ulanRawFilenames = [
   './data/ULAN/raw/ulan_xml_0622/ULAN4.xml',
   './data/ULAN/raw/ulan_xml_0622/ULAN5.xml',
 ];
+
+/**
+ * In an actual production environment, there would be some way to determine
+ * the last succesfully updated datafile, and that file would be returned by
+ * this function.
+ */
+export function getLatestDatafileUrlPath(name: string): string | undefined {
+  if (name === 'collections')
+    return '/data/BrooklynMuseum/collections_20230202_020124.jsonl.gz';
+  if (name === 'content')
+    return '/data/BrooklynMuseum/content_20230202_020124.jsonl.gz';
+  if (name === 'archives')
+    return '/data/BrooklynMuseum/archives_20230202_020124.jsonl.gz';
+}
--- util/data/import/temp/updateDominantColorsCommand.ts
+++ util/data/import/temp/updateDominantColorsCommand.ts
@@ -3,23 +3,39 @@
  *
  * Temporary script, to be removed after the dominant color data is added to the collections data import script.
  *
- * npx ts-node --compiler-options {\"module\":\"CommonJS\"} ./util/data/import/updateHistogramCommand.ts
- */
+ * npx ts-node --compiler-options {\"module\":\"CommonJS\"} ./util/data/import/updateDominantColorsCommand.ts
+
+
+{
+  "id": 121640,
+  "image": "30.1478.25_PS1.jpg",
+  "dominantColorsHsl": [
+    { "h": 17, "s": 38, "l": 43 },
+    { "h": 10, "s": 51, "l": 32 },
+    { "h": 37, "s": 43, "l": 76 },
+    { "h": 19, "s": 30, "l": 50 },
+    { "h": 177, "s": 14, "l": 27 },
+    { "h": 41, "s": 33, "l": 72 }
+  ]
+}
+
+
+*/
 import * as fs from 'fs';
 import { createWriteStream } from 'fs';
 import * as readline from 'node:readline';
-import convert from 'color-convert';
 import Vibrant from 'node-vibrant';
 
 function snooze(s: number) {
   return new Promise((resolve) => setTimeout(resolve, s * 1000));
 }
 
-async function updateHistograms() {
+async function updateDominantColors() {
   const outputStream = createWriteStream(
-    './data/BkM/json/collections.dominant.jsonl'
+    './data/BrooklynMuseum/calculatedFields/dominantColorsHsl.jsonl'
   );
-  const fileStream = fs.createReadStream('./data/BkM/json/collections.jsonl');
+  
+  const fileStream = fs.createReadStream('./data/BrooklynMuseum/collections.jsonl');
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
@@ -49,7 +65,7 @@ async function updateHistograms() {
 }
 
 async function run() {
-  await updateHistograms();
+  await updateDominantColors();
 }
 
 run();
